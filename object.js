0e09169c6b2aa2d943d11319e2992baf20acee29
atnishat <nishat.mu47@gmail.com>
2022-09-02 12:20:57 +0600

    first commit

---
+++ object.js
@@ -0,0 +1,8 @@
+const player = {};
+player.name = 'Small Nirob';
+player.specialty = 'batsman';
+player.bat = function(){
+    console.log('swing your bat');
+}
+// console.log(player);
+// player.bat();
